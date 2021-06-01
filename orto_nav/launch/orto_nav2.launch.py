3b6c53ba41d3b98bf4bd9b230c880d1b06c5e5c8
FraTesta <francesco.testa.ge@gmail.com>
2021-06-01 06:08:36 -0700

    Added the orto_state_publisher launcher, to load the URDF model on Rviz

--- orto_nav/launch/orto_nav2.launch.py
+++ orto_nav/launch/orto_nav2.launch.py
@@ -89,18 +89,18 @@ def generate_launch_description():
         default_value=os.path.join(orto_dir, 'config', 'nav2_params.yaml'),
         description='Full path to the ROS2 parameters file to use for all launched nodes')
     
-    #declare_bt_xml_cmd = DeclareLaunchArgument(
-    #    'default_bt_xml_filename',
-    #    default_value=os.path.join(
-    #        get_package_share_directory('nav2_bt_navigator'),
-    #        'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
-    #    description='Full path to the behavior tree xml file to use')
-    
     declare_bt_xml_cmd = DeclareLaunchArgument(
         'default_bt_xml_filename',
-        default_value=os.path.join(orto_dir,
+        default_value=os.path.join(
+            get_package_share_directory('nav2_bt_navigator'),
             'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
         description='Full path to the behavior tree xml file to use')
+    
+    #declare_bt_xml_cmd = DeclareLaunchArgument(
+    #    'default_bt_xml_filename',
+    #    default_value=os.path.join(orto_dir,
+    #        'behavior_trees', 'navigate_w_replanning_and_recovery.xml'),
+    #    description='Full path to the behavior tree xml file to use')
     
     declare_autostart_cmd = DeclareLaunchArgument(
         'autostart', default_value='true',
